1bf1113a658fb4bb7e427ff3b8ccd86ee90f2de8
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-08-16 16:24:49 +0200

    "Added profile picture, date of birth, and CV fields to Profile model; updated staff index template to display profile picture, added popover and modal functionality"

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(
         User, primary_key=True, on_delete=models.CASCADE, related_name="profile"
     )
+    picture = models.ImageField(upload_to="profile/picture", blank=True, null=True)
     salary_reference_number = models.PositiveIntegerField(blank=True, null=True)
     position = models.ForeignKey(
         Position,
@@ -30,6 +31,8 @@ class Profile(models.Model):
         null=True,
     )
     date_appointed = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    cv = models.FileField(upload_to="profile/cv", blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
---
+++ readme.md
@@ -0,0 +1,9 @@
+### Setup steps
+1.  Create virtual environment and activate it.
+2.  Install the required packages by running `pip install -r requirements.txt` in your terminal
+3.  Create '.env.' file (A template with all the keys are provided)
+4.  Run `python manage.py makemigrations`
+5.  Run `python manage.py migrate`
+6.  Run `python manage.py createsuperuser`
+7.  Run `python manage.py runserver`
+8.  Open your browser and navigate to `http://127.0.0.1:8000
