3efadf5deabd86803affb082816917ca9625a556
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-08-20 08:50:40 +0200

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./templates/**/*.html"],
+	theme: {
+		colors: {
+			primary: {
+				DEFAULT: "#FF0000",
+				50: "#FFC5C5",
+				100: "#FFA8A8",
+				200: "#FF7F7F",
+				300: "#FF6363",
+				400: "#FF4D4D",
+				500: "#FF0000",
+				600: "#CC0000",
+				700: "#B20000",
+				800: "#990000",
+				900: "#7F0000",
+				950: "#660000",
+			},
+		},
+		extend: {},
+	},
+	plugins: [],
+};
