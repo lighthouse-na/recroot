aa735944d1beab12d819b7be05c8834cccc0490f
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-09-18 14:36:46 +0200

    "Updated color schemes in unfold.py and tailwind.config.js to telecom colours"

--- config/settings/third_party/unfold.py
+++ config/settings/third_party/unfold.py
@@ -2,24 +2,39 @@ from django.urls import reverse, reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
 UNFOLD = {
-    "SITE_TITLE": "<Your Title>",
-    "SITE_HEADER": "<Your Title>",
+    "SITE_TITLE": "Telecom Namibia",
+    "SITE_HEADER": "Telecom Namibia",
     "SITE_SYMBOL": "speed",  # symbol from icon set
     "SHOW_HISTORY": True,  # show/hide "History" button, default: True
     "SHOW_VIEW_ON_SITE": True,  # show/hide "View on site" button, default: True
     "COLORS": {
         "primary": {
-            "50": "#FFC5C5",
-            "100": "#FFA8A8",
-            "200": "#FF7F7F",
-            "300": "#FF6363",
-            "400": "#FF4D4D",
-            "500": "#FF0000",
-            "600": "#CC0000",
-            "700": "#B20000",
-            "800": "#990000",
-            "900": "#7F0000",
-            "950": "#660000",
+            "DEFAULT": "#E56114",
+            "50": "#FFF7ED",
+            "100": "#FFEDD5",
+            "200": "#FED7AA",
+            "300": "#FDBA74",
+            "400": "#FB923C",
+            "500": "#F97316",
+            "600": "#EA580C",
+            "700": "#C2410C",
+            "800": "#9A3412",
+            "900": "#7C2D12",
+            "950": "#66240D",
+        },
+        "secondary": {
+            "DEFAULT": "#0057B8",
+            "50": "#F0F5F9",
+            "100": "#D9E2EC",
+            "200": "#A0AEC0",
+            "300": "#6886A0",
+            "400": "#4F748E",
+            "500": "#013569",
+            "600": "#012D5A",
+            "700": "#00274E",
+            "800": "#002145",
+            "900": "#001D3E",
+            "950": "#001833",
         },
     },
     "EXTENSIONS": {
