6edc5ae4b8b1ce9590e92050d66f88b77ec3a131
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-10-04 20:01:21 +0200

    "Updated SMS notification texts for vacancy applications and increased Nginx client max body size to 128M."

--- apps/recruitment/texts.py
+++ apps/recruitment/texts.py
@@ -17,7 +17,8 @@ my_password = str(env("SMS_PASSWORD"))
 def send_vacancy_application_notification_text(instance, created):
     if created and instance.status == "submitted":
         recipient = str(instance.primary_contact)
-        message_body = f"Your application for the {instance.vacancy.title} position at Telecom Namibia has been received. Thank you for choosing Telecom Namibia."
+        # message_body = f"Your application for the {instance.vacancy.title} position at Telecom Namibia has been received. Thank you for choosing Telecom Namibia."
+        message_body = f"Thank you for submitting your application for the {instance.vacancy.title} opportunity at Telecom Namibia. We acknowledge receipt of your application and will carefully assess your qualifications. You will be notified once the review process has been completed.Thank you for your interest in joining Telecom Namibia."
         http_req = (
             f"{my_uri}/api?action=sendmessage"
             f"&username={my_username}"
@@ -38,8 +39,8 @@ def send_vacancy_application_notification_text(instance, created):
 
     if not created and instance.status == "accepted":
         recipient = str(instance.primary_contact)
-        # message_body = f"Your application for the {instance.vacancy.title} at Telecom Namibia has been accepted. Please check your email({instance.email}) inbox or spam folder for more information. Thank you for choosing Telecom Namibia."
-        message_body = f"Congratulations! Your application for the {instance.vacancy.title} position at Telecom Namibia has been successful. We'll be in touch soon with more details. Thank you for choosing Telecom Namibia."
+        # message_body = f"Congratulations! Your application for the {instance.vacancy.title} position at Telecom Namibia has been successful. We'll be in touch soon with more details. Thank you for choosing Telecom Namibia."
+        message_body = f"Thank you for submitting your application for the {instance.vacancy.title} opportunity at Telecom Namibia. Your application has successfully met the minimum criteria for further assessment. We will review your submission and inform you of the next steps in due course. We appreciate your interest in a career with Telecom Namibia."
         http_req = (
             f"{my_uri}/api?action=sendmessage"
             f"&username={my_username}"
@@ -60,8 +61,8 @@ def send_vacancy_application_notification_text(instance, created):
 
     if not created and instance.status == "rejected":
         recipient = str(instance.primary_contact)
-        # message_body = f"Your application for the {instance.vacancy.title} at Telecom Namibia has been rejected. Please check your email({instance.email}) inbox or spam folder for more information."
-        message_body = f"Unfortunately, your application for the {instance.vacancy.title} position hasn't been selected. We appreciate you considering Telecom Namibia. We wish you all the best in your job search."
+        # message_body = f"Unfortunately, your application for the {instance.vacancy.title} position hasn't been selected. We appreciate you considering Telecom Namibia. We wish you all the best in your job search."
+        message_body = f"Thank you for your application for the {instance.vacancy.title} opportunity at Telecom Namibia. After a thorough review, we regret to inform you that your application does not meet the minimum criteria. We value your interest in Telecom Namibia and encourage you to apply for future opportunities. Wishing you success in your job search."
         http_req = (
             f"{my_uri}/api?action=sendmessage"
             f"&username={my_username}"
